fe9c44eafbccfed5d341e1944c23b2897db000f7
Harry Reeder <harry@harryreeder.co.uk>
2014-08-14 17:27:56 +0100

    add assets to manage£

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+from flask.ext.assets import ManageAssets
 from flask.ext.script import Manager
-from intranet import app
+from intranet import app, assets
 
 manager = Manager(app)
+manager.add_command("assets", ManageAssets(assets))
 
 if __name__ == "__main__":
 	manager.run()
