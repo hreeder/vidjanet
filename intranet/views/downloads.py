4ad7d5e76bdfca5d49e60ab5afe7abaaa1b4b2e7
Harry Reeder <harry@harryreeder.co.uk>
2014-10-28 17:11:29 +0100

    Add download tag handling to main view

--- intranet/views/downloads.py
+++ intranet/views/downloads.py
@@ -1,7 +1,15 @@
 from intranet import app
+from intranet.models.downloads import DownloadFile, DownloadTag
 
 from flask import render_template
 
 @app.route("/downloads")
-def downloads_index():
-	return render_template("downloads.html")
+@app.route("/downloads/tagged/<tagname>")
+def downloads_index(tagname=None):
+	tags = DownloadTag.query.all()
+	if tagname:
+		tag = DownloadTag.query.filter_by(name=tagname).first_or_404()
+		downloads = tag.files.all()
+	else:
+		downloads = DownloadFile.query.all()
+	return render_template("downloads.html", downloads=downloads, tags=tags)
