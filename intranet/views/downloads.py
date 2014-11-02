3ed40409857ee1290797f2f35106430153d5b3a0
Harry Reeder <harry@harryreeder.co.uk>
2014-11-02 11:47:56 +0000

    order tags alphabetically

--- intranet/views/downloads.py
+++ intranet/views/downloads.py
@@ -6,7 +6,7 @@ from flask import render_template
 @app.route("/downloads")
 @app.route("/downloads/tagged/<tagname>")
 def downloads_index(tagname=None):
-	tags = DownloadTag.query.all()
+	tags = DownloadTag.query.order_by(DownloadTag.name).all()
 	if tagname:
 		tag = DownloadTag.query.filter_by(name=tagname).first_or_404()
 		downloads = tag.files.all()
